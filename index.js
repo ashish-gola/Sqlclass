8be03f1634cf77d7a50d1790239b72f706b94676
Ashish Gola <golaashish036@gmail.com>
2025-03-02 22:45:00 +0530

    class commit

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: 'Ashish@12'
+});
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if(err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
 
 let getRandomUser = () => {
     return {
@@ -9,4 +30,4 @@ let getRandomUser = () => {
     };
   }
 
-  console.log(getRandomUser());
+  // console.log(getRandomUser());
